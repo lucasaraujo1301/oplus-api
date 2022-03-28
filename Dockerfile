0fc81ec23161f7923373545b74ccb8712aaf5d30
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2022-03-27 21:22:26 -0300

    Configurating Docker and Postgres database.

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.8-alpine
+MAINTAINER Lucas Araujo
+
+ENV PYTHONUNBUFFERED 1
+
+ENV VIRTUAL_ENV=/py
+RUN python3 -m venv $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
+COPY ./requirements.txt /requirements.txt
+COPY . /app
+
+WORKDIR /app
+
+RUN pip install --upgrade pip && \
+    apk add --update --no-cache postgresql-client &&\
+    apk add --update --no-cache --virtual .tmp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev &&\
+    pip install -r /requirements.txt &&\
+    apk del .tmp-build-deps
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- oplus_api/settings.py
+++ oplus_api/settings.py
@@ -10,22 +10,31 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9a3eu=m@fjoiv38_bqf(#2&_ar1dm6eiudsn!zuadq_53m7b=1'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+print(DEBUG)
+
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -75,8 +84,12 @@ WSGI_APPLICATION = 'oplus_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': env('DB_HOST'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
+        'PORT': env('DB_PORT')
     }
 }
 
