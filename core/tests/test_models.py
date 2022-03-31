b3baca8fc86a47486a073b9f90025e29b3873053
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2022-03-31 16:36:52 -0300

    Added test to created_at on test_models

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -4,6 +4,7 @@ import datetime
 from core.models import Patient
 
 
+@pytest.mark.freeze_time(datetime.date.today())
 @pytest.mark.django_db
 def test_patient_model():
     patient = Patient(
@@ -20,5 +21,6 @@ def test_patient_model():
     assert patient.birth_date == datetime.date.today()
     assert patient.birth_date_formate() == datetime.date.today().strftime('%d/%m/%Y')
 
-    assert patient.created_at
+    assert patient.created_at.strftime('%d/%m/%Y %H:%M:%S %f') == datetime.datetime.now().strftime(
+        '%d/%m/%Y %H:%M:%S %f')
     assert str(patient) == patient.name
